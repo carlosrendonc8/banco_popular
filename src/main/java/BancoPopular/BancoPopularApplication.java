2f2d6009d6a56a95c6e22afa0042a441ce6d7857
Aprendiz <aprendiz@IBAAPRRTLFSM009.local>
2023-10-02 10:30:46 -0500

    Inicializacion del banco

---
+++ src/main/java/BancoPopular/BancoPopularApplication.java
@@ -0,0 +1,13 @@
+package BancoPopular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoPopularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoPopularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/BancoPopular/BancoPopularApplicationTests.java
@@ -0,0 +1,13 @@
+package BancoPopular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoPopularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
